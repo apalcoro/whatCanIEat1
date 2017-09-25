e25eb682cac72694dfc4f65f9e3f5d34e2f502a9
Almog_Shemesh <almog_52@walla.com>
2017-09-25 15:56:05 +0300

    up1

--- app/src/main/java/com/coral/whatcanieat/SignUp.java
+++ app/src/main/java/com/coral/whatcanieat/SignUp.java
@@ -13,46 +13,47 @@ import com.backendless.async.callback.AsyncCallback;
 import com.backendless.exceptions.BackendlessFault;
 
 public class SignUp extends AppCompatActivity {
-EditText email;
+    EditText email;
     EditText pass;
     EditText name;
-    Button save;
+    Button signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        email = (EditText)findViewById(R.id.email);
-        pass = (EditText)findViewById(R.id.pass);
-        name = (EditText)findViewById(R.id.name);
-        save = (Button)findViewById(R.id.save);
+        email = (EditText) findViewById(R.id.email);
+        pass = (EditText) findViewById(R.id.pass);
+        name = (EditText) findViewById(R.id.name);
+        signup = (Button) findViewById(R.id.signup);
         //
-        Backendless.initApp( this,
+        Backendless.initApp(this,
                 Defaults.APPLICATION_ID,
                 Defaults.API_KEY);
 
     }
-    public void SignUp(View view){
-        save.setOnClickListener(new View.OnClickListener() {
+
+    public void CreateAccount(View view) {
+        signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 BackendlessUser user = new BackendlessUser();
-                user.setProperty( "email",email.getText().toString());
-                user.setProperty( "name",name.getText().toString());
+                user.setProperty("email", email.getText().toString());
+                user.setProperty("name", name.getText().toString());
                 user.setPassword(pass.getText().toString());
-                Backendless.UserService.register( user, new AsyncCallback<BackendlessUser>()
-                {
-                    public void handleResponse( BackendlessUser registeredUser )
-                    {
-                        Toast.makeText(SignUp.this,"Registration Complete",Toast.LENGTH_LONG).show();
+                Backendless.UserService.register(user, new AsyncCallback<BackendlessUser>() {
+                    public void handleResponse(BackendlessUser registeredUser) {
+                        Toast.makeText(SignUp.this, "Registration Complete", Toast.LENGTH_LONG).show();
                     }
 
-                    public void handleFault( BackendlessFault fault )
-                    {
-                        Toast.makeText(SignUp.this,"Fail to register",Toast.LENGTH_LONG).show();
+                    public void handleFault(BackendlessFault fault) {
+                        Toast.makeText(SignUp.this, "Fail to register", Toast.LENGTH_LONG).show();
                     }
-                } );
+                });
             }
         });
     }
 }
+
+
